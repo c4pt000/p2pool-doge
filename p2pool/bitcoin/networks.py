280a844b12e20fe0f8f81b900156b7460756e03c
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-05-28 02:12:54 -0400

    Update networks.py

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -229,6 +229,27 @@ nets = dict(
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=0.03e8,
     ),
+    radiocoin=math.Object(
+        P2P_PREFIX='c0c0c0c0'.decode('hex'),
+        P2P_PORT=22556,
+        ADDRESS_VERSION=30,
+        RPC_PORT=22555,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'radiocoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 500000*100000000 >> (height + 1)//100000, #not bullet proof bot will be ok for now
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=60, # s
+        SYMBOL='RADC',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'RadioCoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Radiocoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.radiocoin'), 'radiocoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://radiochain.info/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://radiochain.info/address/',
+        TX_EXPLORER_URL_PREFIX='http://radiochain.info/tx/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=0.03e8,
+    ),
     
 )
 for net_name, net in nets.iteritems():
